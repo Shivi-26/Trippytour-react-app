c2a93c5f12aeddd2c8db046dff90b5d6e7945a72
Shivangi <107808440+Shivi-26@users.noreply.github.com>
2024-01-12 19:57:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,15 +4,16 @@ const fs = require('fs');
 const app = express();
 const cors = require('cors');
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors({ origin: 'https://trippytour-react-app.vercel.app' })); // Set the correct origin
 
+// Create an API endpoint to fetch data
 app.get('/api/data', (req, res) => {
   const results = [];
   fs.createReadStream('hotel_bookings_1000.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.setHeader('Access-Control-Allow-Origin', 'https://trippytour-react-app.vercel.app');
       res.json(results);
     });
 });
