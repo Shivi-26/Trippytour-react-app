1911e99f37c8abb1b2a9115dea81dff79c8922cf
shivi-26 <cseshivangi66@gmail.com>
2024-01-13 14:00:19 +0530

    changes trippy-tour

--- server/server.js
+++ server/server.js
@@ -6,14 +6,13 @@ const cors = require('cors');
 
 app.use(cors()); // Enable CORS for all routes
 
-// Create an API endpoint to fetch data
-app.get('/api/data', cors(), (req, res) => {
+app.get('/api/data', (req, res) => {
   const results = [];
   fs.createReadStream('hotel_bookings_1000.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Set the Access-Control-Allow-Origin header
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.json(results);
     });
 });
